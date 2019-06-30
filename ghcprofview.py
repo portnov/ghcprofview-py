c37216af7110ec8e3b5ed17cbc73a5fff8bbe09a
Ilya Portnov <portnov@bk.ru>
2019-06-30 22:46:04 +0500

    Implement filters.

--- ghcprofview.py
+++ ghcprofview.py
@@ -5,7 +5,12 @@ import sys
 from PyQt5.QtGui import QPainter, QPixmap, QIcon, QStandardItemModel, QStandardItem, QColor
 from PyQt5 import QtCore
 from PyQt5.QtCore import QRect, QSize, Qt, QObject, QTimer, pyqtSignal, QSettings, QModelIndex, QVariant, QAbstractItemModel, QSortFilterProxyModel, QItemSelectionModel
-from PyQt5.QtWidgets import QApplication, QWidget, QToolBar, QMainWindow, QDialog, QVBoxLayout, QHBoxLayout, QAction, QActionGroup, QLabel, QFileDialog, QFrame, QDockWidget, QMessageBox, QListWidget, QListWidgetItem, QMenu, QTreeView, QLineEdit, QPushButton, QAbstractItemView, QStyle, QStyledItemDelegate
+from PyQt5.QtWidgets import QApplication, QWidget, QToolBar, QMainWindow, \
+        QDialog, QVBoxLayout, QHBoxLayout, QAction, QActionGroup, QLabel, QFileDialog, \
+        QFrame, QDockWidget, QMessageBox, QListWidget, QListWidgetItem, QMenu, \
+        QSpinBox, \
+        QTreeView, QLineEdit, QPushButton, QAbstractItemView, QStyle, \
+        QStyledItemDelegate
 
 column_names = ["Name", "Entries",
                 "Time Individual %", "Alloc Individual %",
@@ -150,8 +155,8 @@ def print_table(table):
         print_record(record, 0)
 
 def percent_color(value):
-    zero = QColor(128, 255, 128)
-    one = QColor(255, 128, 128)
+    zero = QColor.fromHsv(111, 100, 190)
+    one = QColor.fromHsv(5, 100, 190)
 
     return QColor((1 - value) * zero.red() + value * one.red(),
                   (1 - value) * zero.green() + value * one.green(),
@@ -159,7 +164,7 @@ def percent_color(value):
 
 class PercentDelegate(QStyledItemDelegate):
     def paint(self, painter, option, index):
-        QStyledItemDelegate.paint(self, painter, option, index)
+        #QStyledItemDelegate.paint(self, painter, option, index)
         painter.save()
         if option.state & QStyle.State_Selected:
             painter.fillRect(option.rect, option.palette.highlight())
@@ -234,6 +239,8 @@ class DataModel(QAbstractItemModel):
             return item.data(index.column())
         elif role == QtCore.Qt.DisplayRole:
             return item.data(index.column())
+        elif role == QtCore.Qt.UserRole + 1:
+            return item
         else:
             return QVariant()
 
@@ -245,12 +252,88 @@ class DataModel(QAbstractItemModel):
         else:
             return QVariant()
 
+class FilterModel(QSortFilterProxyModel):
+    def __init__(self, parent):
+        QSortFilterProxyModel.__init__(self, parent)
+        self.individual_time = None
+        self.individual_alloc = None
+        self.inherited_time = None
+        self.inherited_alloc = None
+        self.name = None
+
+    def check(self, sourceRow, sourceParent):
+        idx = self.sourceModel().index(sourceRow, 0, sourceParent)
+        if not idx.isValid():
+            return False
+        record = self.sourceModel().data(idx, QtCore.Qt.UserRole + 1)
+
+        if self.individual_time is not None and self.individual_time > record.individual_time:
+            return False
+
+        if self.individual_alloc is not None and self.individual_alloc > record.individual_alloc:
+            return False
+
+        if self.inherited_time is not None and self.inherited_time > record.inherited_time:
+            return False
+
+        if self.inherited_alloc is not None and self.inherited_alloc > record.inherited_alloc:
+            return False
+
+        if self.name and self.name not in record.name:
+            return False
+
+        return True
+
+    def filterAcceptsRow(self, sourceRow, sourceParent):
+        if self.check(sourceRow, sourceParent):
+            return True
+
+        parent = sourceParent
+        while parent and parent.isValid():
+            if self.check(parent.row(), parent.parent()):
+                return True
+            parent = parent.parent()
+
+        if self.hasAcceptedChildren(sourceRow, sourceParent):
+            return True
+
+        return False
+
+    def hasAcceptedChildren(self, sourceRow, sourceParent):
+        item = self.sourceModel().index(sourceRow, 0, sourceParent)
+        if not item.isValid():
+            return False
+
+        childCount = item.model().rowCount(item)
+        for row in range(childCount):
+            if self.check(row, item):
+                return True
+            if self.hasAcceptedChildren(row, item):
+                return True
+        return False
+
+    def setFilter(self, name, individual_time, individual_alloc, inherited_time, inherited_alloc):
+        self.name = name
+        self.inherited_time = inherited_time
+        self.inherited_alloc = inherited_alloc
+        self.individual_time = individual_time
+        self.individual_alloc = individual_alloc
+        self.invalidateFilter()
+
+    def reset(self):
+        self.name = None
+        self.inherited_time = None
+        self.inherited_alloc = None
+        self.individual_time = None
+        self.individual_alloc = None
+        self.invalidateFilter()
+
 class Viewer(QMainWindow):
     def __init__(self, table):
         QMainWindow.__init__(self)
         self.tree = QTreeView(self)
         self.model = DataModel(table)
-        self.sorter = sorter = QSortFilterProxyModel(self)
+        self.sorter = sorter = FilterModel(self)
         sorter.setSourceModel(self.model)
         self.tree.setModel(sorter)
         for col in range(2,8):
@@ -261,15 +344,57 @@ class Viewer(QMainWindow):
         self.tree.expand(self.sorter.index(0,0))
         #self.tree.expandAll()
 
-        hbox = QHBoxLayout()
+        searchbox = QHBoxLayout()
         self.search = QLineEdit(self)
-        btn = QPushButton("Search", self)
-        hbox.addWidget(self.search)
-        hbox.addWidget(btn)
+        btn = QPushButton("&Search", self)
+        searchbox.addWidget(self.search)
+        searchbox.addWidget(btn)
         btn.clicked.connect(self._on_search)
 
+        filterbox = QHBoxLayout()
+
+        label = QLabel("Time Individual", self)
+        filterbox.addWidget(label)
+        self.individual_time = QSpinBox(self)
+        self.individual_time.setMinimum(0)
+        self.individual_time.setMaximum(100)
+        self.individual_time.setSuffix(" %")
+        filterbox.addWidget(self.individual_time)
+
+        label = QLabel("Alloc Individual", self)
+        filterbox.addWidget(label)
+        self.individual_alloc = QSpinBox(self)
+        self.individual_alloc.setMinimum(0)
+        self.individual_alloc.setMaximum(100)
+        self.individual_alloc.setSuffix(" %")
+        filterbox.addWidget(self.individual_alloc)
+
+        label = QLabel("Time Inherited", self)
+        filterbox.addWidget(label)
+        self.inherited_time = QSpinBox(self)
+        self.inherited_time.setMinimum(0)
+        self.inherited_time.setMaximum(100)
+        self.inherited_time.setSuffix(" %")
+        filterbox.addWidget(self.inherited_time)
+
+        label = QLabel("Alloc Inherited", self)
+        filterbox.addWidget(label)
+        self.inherited_alloc = QSpinBox(self)
+        self.inherited_alloc.setMinimum(0)
+        self.inherited_alloc.setMaximum(100)
+        self.inherited_alloc.setSuffix(" %")
+        filterbox.addWidget(self.inherited_alloc)
+
+        btn = QPushButton("&Filter", self)
+        btn.clicked.connect(self._on_filter)
+        filterbox.addWidget(btn)
+        btn = QPushButton("&Reset", self)
+        filterbox.addWidget(btn)
+        btn.clicked.connect(self._on_reset_filter)
+
         vbox = QVBoxLayout()
-        vbox.addLayout(hbox)
+        vbox.addLayout(searchbox)
+        vbox.addLayout(filterbox)
         vbox.addWidget(self.tree)
         widget = QWidget(self)
         widget.setLayout(vbox)
@@ -284,7 +409,7 @@ class Viewer(QMainWindow):
         #print(idxs)
         for idx in reversed(idxs[:-1]):
             data = self.sorter.data(idx, QtCore.Qt.DisplayRole)
-            print(data)
+            #print(data)
             self.tree.expand(idx)
 
     def _on_search(self):
@@ -300,6 +425,12 @@ class Viewer(QMainWindow):
             self._expand_to(idxs[0])
             self.tree.selectionModel().select(idxs[0], QItemSelectionModel.ClearAndSelect | QItemSelectionModel.Current | QItemSelectionModel.Rows)
             self.tree.scrollTo(idxs[0], QAbstractItemView.PositionAtCenter)
+
+    def _on_filter(self):
+        self.sorter.setFilter(self.search.text(), self.individual_time.value(), self.individual_alloc.value(), self.inherited_time.value(), self.inherited_alloc.value())
+
+    def _on_reset_filter(self):
+        self.sorter.reset()
 
 if __name__ == "__main__":
     path = sys.argv[1]
