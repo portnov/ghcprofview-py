ec65ac9ee857d8494081fe776c1c6393fe7f11f2
Chris Wendt <chrismwendt@gmail.com>
2023-03-10 17:24:34 -0700

    qt6 migration: QAction, QActionGroup

--- ghcprofview.py
+++ ghcprofview.py
@@ -4,11 +4,12 @@ import sys
 import re
 import traceback
 
-from PyQt6.QtGui import QPainter, QPixmap, QIcon, QStandardItemModel, QStandardItem, QColor
+from PyQt6.QtGui import QPainter, QPixmap, QIcon, QStandardItemModel, QStandardItem, QColor, \
+        QAction, QActionGroup
 from PyQt6 import QtCore
 from PyQt6.QtCore import QRect, QSize, Qt, QObject, QTimer, pyqtSignal, QSettings, QModelIndex, QVariant, QAbstractItemModel, QSortFilterProxyModel, QItemSelectionModel
 from PyQt6.QtWidgets import QApplication, QWidget, QToolBar, QMainWindow, \
-        QDialog, QVBoxLayout, QHBoxLayout, QAction, QActionGroup, QLabel, QFileDialog, \
+        QDialog, QVBoxLayout, QHBoxLayout, QLabel, QFileDialog, \
         QFrame, QDockWidget, QMessageBox, QListWidget, QListWidgetItem, QMenu, \
         QSpinBox, QComboBox, \
         QTreeView, QLineEdit, QPushButton, QAbstractItemView, QStyle, \
